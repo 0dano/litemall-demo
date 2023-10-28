f1fa8eea06fddcbe58b4f237eb67a26058accb2c
朱杰 <zhujie@xforceplus.com>
2023-10-28 08:57:39 +0800

    db modify

--- litemall-db/sql/litemall_schema.sql
+++ litemall-db/sql/litemall_schema.sql
@@ -1,8 +1,8 @@
-drop database if exists litemall;
-drop user if exists 'litemall'@'%';
+drop database if exists litemall_demo;
+drop user if exists 'litemall_demo'@'%';
 -- 支持emoji：需要mysql数据库参数： character_set_server=utf8mb4
-create database litemall default character set utf8mb4 collate utf8mb4_unicode_ci;
-use litemall;
-create user 'litemall'@'%' identified by 'litemall123456';
-grant all privileges on litemall.* to 'litemall'@'%';
+create database litemall_demo default character set utf8mb4 collate utf8mb4_unicode_ci;
+use litemall_demo;
+create user 'litemall_demo'@'%' identified by 'litemall123456';
+grant all privileges on litemall_demo.* to 'litemall_demo'@'%';
 flush privileges;
