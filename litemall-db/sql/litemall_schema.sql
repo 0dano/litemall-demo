5a7c5939aaa241f5766936d16eae9ac65f427c97
朱杰 <zhujie@xforceplus.com>
2023-10-28 10:04:51 +0800

    db password modify

--- litemall-db/sql/litemall_schema.sql
+++ litemall-db/sql/litemall_schema.sql
@@ -3,6 +3,6 @@ drop user if exists 'litemall_demo'@'%';
 -- 支持emoji：需要mysql数据库参数： character_set_server=utf8mb4
 create database litemall_demo default character set utf8mb4 collate utf8mb4_unicode_ci;
 use litemall_demo;
-create user 'litemall_demo'@'%' identified by 'litemall123456';
+create user 'litemall_demo'@'%' identified by 'Test!!!!1234';
 grant all privileges on litemall_demo.* to 'litemall_demo'@'%';
 flush privileges;
